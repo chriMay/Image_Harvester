239cfc1470d2150a886431cd066e0ae2f8b2de50
chriMay <christoph-mayer@hotmail.com>
2022-02-06 13:03:42 +0100

    renamed gui file

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # !/usr/bin/python3
 
 from mvIMPACT.acquire import DeviceManager
-from main_ui import HarvesterInterface
+from gui import HarvesterInterface
 
 if __name__ == "__main__":
     devMgr = DeviceManager()
